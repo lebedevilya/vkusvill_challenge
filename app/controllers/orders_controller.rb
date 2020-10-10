04c8fa48902efc2a7c7b8eaccd4e72a9dcaa7eac
Ilya Lebedev <ilyalebedev1@yandex.ru>
2020-10-10 18:55:48 +0300

    feat: orders controller

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+class OrdersController < ApplicationController
+  before_action :set_order, only: [:show]
+
+  def index
+    @orders = Order.all
+  end
+
+  def show
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_order
+    @order = Order.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def order_params
+    params.fetch(:order, {})
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :orders, only: [:index, :show]
 end
