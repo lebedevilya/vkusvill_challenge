81ed9dee961ff9c9ac5ebddad0ce5dad075224bc
Ilya Lebedev <ilyalebedev1@yandex.ru>
2020-10-10 22:34:38 +0300

    fix: item creation

--- app/admin/items.rb
+++ app/admin/items.rb
@@ -1,20 +1,5 @@
 # frozen_string_literal: true
 
 ActiveAdmin.register Item do
-
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :order_id, :product_id, :count, :weight, :picker_count, :picker_weight, :status
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:order_id, :product_id, :count, :weight, :picker_count, :picker_weight, :status]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-
+  permit_params :order_id, :product_id, :count, :weight, :picker_weight, :status
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -14,9 +14,9 @@
 #  updated_at    :datetime         not null
 #
 # frozen_string_literal: true
+
 class Item < ApplicationRecord
   belongs_to :order
   belongs_to :product
   enum status: [:created, :in_progress, :finished]
-
 end
